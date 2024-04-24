055120ced8a779eb797088fa6dcc92b42c03afc8
Gustavo Henrique Calanzani <ghc.calanzani@gmail.com>
2024-04-24 19:23:36 -0300

    update

--- Tópicos Especiais/src/pilha_fila/PilhaItemLse.java
+++ Tópicos Especiais/src/pilha_fila/PilhaItemLse.java
@@ -1,5 +1,12 @@
 package pilha_fila;
 
 public class PilhaItemLse {
+    private NoItem top;
+    private int quant;
+
+    public PilhaItemLse() {
+        this.top = null;
+        this.quant = 0;
+    }
     
 }
