3b60c7753e6b0172396b4d605727f42891c4bfe5
Gustavo Henrique Calanzani <ghc.calanzani@gmail.com>
2024-05-12 18:57:30 -0300

    working

--- Compiladores/main.c
+++ Compiladores/main.c
@@ -2,47 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct no{
+    char token[20];
+    struct no *prox;
+};
+
 int main() {
+    struct no *p, *head[3];
+    int i, tipotoken;
+    char tokenLido[20];
+    FILE *arquivo;
 
-    struct no{
-        int dado;
-        struct no *prox;
-    };
-
-    struct no *p, *head[5];
-    int i, aux, tipotoken;
-
-    for(i=0; i<5; i++){
+    for(i=0; i<3; i++){
         head[i] = NULL;
     }
+    //Tipo 0 = token tipo de dado (int, float, long e char")
+    //Tipo 1 = token valor (0, 0-2147483647)
+    //Tipo 2 = tokens não reconhecidos
 
-    do{
-        //Rodar teste de automatos
-        printf("Informe o tipo de token a incluir (0-4): ");
-        scanf("%d", &tipotoken);
-
-        //Inclusão de lexema na lista correspondente
-        p = malloc(sizeof(struct no));
-        p->prox = head[tipotoken];
-        printf("Digite o lexema: ");
-        scanf("%i", &p->dado);
-        head[tipotoken] = p;
-
-        printf("Deseja inserir um novo lexema: (0-nao 1-sim)");
-        scanf("%i", &aux);
-
-    } while(aux == 1);
-
-    //Printar elementos
-    printf("\nElementos:\n");
-    for (i = 0; i < 5; i++) {
-        p = head[i];
-        while (p != NULL) {
-            printf("%d ", p->dado);
-            p = p->prox;
-        }
-        printf("\n");
+    arquivo = fopen("../input.txt", "r");
+    if (arquivo == NULL){
+        printf("Não foi possível abrir o arquivo.\n");
+        return 1;
     }
 
+    while(fscanf(arquivo, "%[^;];", tokenLido) != EOF){
+        p = malloc(sizeof(struct no));
+        strcpy(p->token, tokenLido);
+
+        if(strcmp(tokenLido, "int") == 0 || strcmp(tokenLido, "float") == 0 || strcmp(tokenLido, "double") == 0 || strcmp(tokenLido, "char") == 0){
+            tipotoken = 0;
+        }
+        else if((strchr(tokenLido, '0') != NULL && strlen(tokenLido) == 1) || atoi(tokenLido) != 0) {
+            tipotoken = 1;
+        } else {
+            tipotoken = 2;
+        }
+
+        p->prox = head[tipotoken];
+        head[tipotoken] = p;
+    }
+
+    //Printar elementos
+     printf("\nElementos:\n");
+     for (i = 0; i < 3; i++) {
+         p = head[i];
+         while (p != NULL) {
+             printf("%s ", p->token);
+             p = p->prox;
+         }
+         printf("\n");
+     }
+    
+    fclose(arquivo);
     return 0;
 }
