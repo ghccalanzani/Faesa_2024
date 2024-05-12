1aa1a40dab14fb1126f257cd315d674d09ee9c76
Gustavo Henrique Calanzani <ghc.calanzani@gmail.com>
2024-05-12 18:42:31 -0300

    input

--- Compiladores/main2.c
+++ Compiladores/main2.c
@@ -8,16 +8,17 @@ struct no{
 };
 
 int main() {
-    struct no *p, *head[2];
+    struct no *p, *head[3];
     int i, tipotoken;
     char tokenLido[20];
     FILE *arquivo;
 
-    for(i=0; i<2; i++){
+    for(i=0; i<3; i++){
         head[i] = NULL;
     }
     //Tipo 0 = token tipo de dado (int, float, long e char")
     //Tipo 1 = token valor (0, 0-2147483647)
+    //Tipo 2 = tokens não reconhecidos
 
     arquivo = fopen("input.txt", "r");
     if (arquivo == NULL){
@@ -32,13 +33,27 @@ int main() {
         if(strcmp(tokenLido, "int") == 0 || strcmp(tokenLido, "float") == 0 || strcmp(tokenLido, "double") == 0 || strcmp(tokenLido, "char") == 0){
             tipotoken = 0;
         }
-        if((strchr(tokenLido, '0') != NULL && strlen(tokenLido) == 1) || atoi(tokenLido) != 0) {
+        else if((strchr(tokenLido, '0') != NULL && strlen(tokenLido) == 1) || atoi(tokenLido) != 0) {
             tipotoken = 1;
+        } else {
+            tipotoken = 2;
         }
 
         p->prox = head[tipotoken];
         head[tipotoken] = p;
     }
+
+    //Printar elementos
+    printf("\nElementos:\n");
+    for (i = 0; i < 3; i++) {
+        p = head[i];
+        while (p != NULL) {
+            printf("%s ", p->token);
+            p = p->prox;
+        }
+        printf("\n");
+    }
+
 
     fclose(arquivo);
     return 0;
